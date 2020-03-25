0e027e5b36eb0df7998a15e6e74160ab3a382082
Yuzunoha <studysteadystandby@gmail.com>
2020-03-25 14:10:18 +0900

    なんだ簡単じゃんdocker exec secondary_db_1 whoami

--- main.sh
+++ main.sh
@@ -4,9 +4,12 @@ readonly BASE_PATH='/home/yuzunoha/git/docker-nextcloud1'
 readonly NEXTCLOUD_DIR_P=$BASE_PATH'/primary/nextcloud/'
 readonly NEXTCLOUD_DIR_S=$BASE_PATH'/secondary/nextcloud'
 
+secondary_db_1
+
 echo $NEXTCLOUD_DIR_P
 echo $NEXTCLOUD_DIR_S
 
+docker exec secondary_db_1 whoami
 
 cd $NEXTCLOUD_DIR_P
 pwd

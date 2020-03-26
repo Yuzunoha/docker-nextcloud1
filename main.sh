bec90632b3e4e56c2d27cd22fd8a57813de46907
Yuzunoha <studysteadystandby@gmail.com>
2020-03-27 02:22:58 +0900

    rsyncはスラッシュなしだと動かなかった。つらい

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 定数宣言
-readonly BASE_DIR='/home/yuzunoha/git/docker-nextcloud1'
-readonly ROOT_DIR_P=$BASE_DIR'/docker/primary'
-readonly ROOT_DIR_S=$BASE_DIR'/docker/secondary'
-readonly NEXTCLOUD_DIR_P=$ROOT_DIR_P'/nextcloud'
-readonly NEXTCLOUD_DIR_S=$ROOT_DIR_S'/nextcloud'
+readonly BASE_PATH='/home/yuzunoha/git/docker-nextcloud1/'
+readonly ROOT_PATH_P=$BASE_PATH'docker/primary/'
+readonly ROOT_PATH_S=$BASE_PATH'docker/secondary/'
+readonly NEXTCLOUD_PATH_P=$ROOT_PATH_P'/nextcloud/'
+readonly NEXTCLOUD_PATH_S=$ROOT_PATH_S'/nextcloud/'
 readonly SQL_DUMP_FILE_NAME='nextcloud-sqlbkp.bak'
 readonly DB_CONTAINER_NAME_P='primary_db_1'
 readonly DB_CONTAINER_NAME_S='secondary_db_1'
@@ -13,16 +13,16 @@ readonly APP_CONTAINER_NAME_P='primary_app_1'
 readonly APP_CONTAINER_NAME_S='secondary_app_1'
 
 echo 'ベースディレクトリに移動する'
-cd $BASE_DIR
+cd $BASE_PATH
 
 echo 'プライマリのデータフォルダをセカンダリにコピーする'
-rsync -Aax $NEXTCLOUD_DIR_P $NEXTCLOUD_DIR_S
+rsync -Aax $NEXTCLOUD_PATH_P $NEXTCLOUD_PATH_S
 
 echo 'プライマリのdbをsqlファイルにエクスポートする'
 docker exec $DB_CONTAINER_NAME_P mysqldump --single-transaction -u root -proot nextcloud > ./$SQL_DUMP_FILE_NAME
 
 echo 'sqlファイルをセカンダリのdbコンテナに転送する'
-docker cp ./$SQL_DUMP_FILE_NAME $DB_CONTAINER_NAME_S/:/dmp
+docker cp ./$SQL_DUMP_FILE_NAME $DB_CONTAINER_NAME_S:/dmp
 
 echo 'セカンダリのdbコンテナで既存のDBを削除する'
 docker exec $DB_CONTAINER_NAME_S sh -c 'mysql -u nextcloud -pnextcloud -e "DROP DATABASE nextcloud"'
